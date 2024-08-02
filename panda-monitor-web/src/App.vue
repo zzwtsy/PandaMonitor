2445e49820c14c98dfe5f6d03ca9d16027bbd174
zzwtsy <zzwtsy@yumdeb.top>
2024-08-02 21:22:40 +0800

    style(App)：调整Toaster组件的顶部外边距
    将Toaster组件的顶部外边距从2rem调整为2.5rem

--- panda-monitor-web/src/App.vue
+++ panda-monitor-web/src/App.vue
@@ -9,5 +9,5 @@ import { Toaster } from '@/components/ui/sonner'
   <main class="md:container px-1 mt-2">
     <DashboardView />
   </main>
-  <Toaster close-button rich-colors position="top-right" style="margin-top: 2rem" />
+  <Toaster close-button rich-colors position="top-right" style="margin-top: 2.5rem" />
 </template>
